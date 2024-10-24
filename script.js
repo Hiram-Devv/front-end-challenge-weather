a1fc007adcca26d7f19976ac85d41aafc61cdc59
HiramDev <hiramdev02@gmail.com>
2024-10-24 17:37:30 -0600

    Agregué funcionalidad para mostrar clima en el html

--- script.js
+++ script.js
@@ -61,19 +61,66 @@ function consultarAPI(ciudad, pais){
     fetch(url)
         .then(respuesta => respuesta.json())
         .then(datos => {
-            console.log(datos)
+
+            limpiarHTML(); // Limpiar html previo
+            
             if(datos.cod === '404'){
                 mostrarError('Ciudad no encontrada')
                 return;
             }
 
-            
                 // Imprimir la respuesta en el html
                 mostrarClima(datos);
         })
 }
 
 
+function mostrarClima(datos){
+    const { name, main: {temp, temp_max, temp_min}} = datos
+
+    const centigrados = kelvinACentigrados(temp);
+    const temp_Maxima = kelvinACentigrados(temp_max);
+    const temp_Minima = kelvinACentigrados(temp_min);
+    
+    // Mostrar en HTML
+    // Nombre de la ciudad
+    const nombreCiudad = document.createElement('h1');
+    nombreCiudad.classList.add('nombre-ciudad');
+    nombreCiudad.textContent = name
+    // Temperatura actual
+    const tempActual = document.createElement('p');
+    tempActual.classList.add('style-temp-act');
+    tempActual.innerHTML = `${centigrados} &#8451`;
+
+    // Temperatura máxima
+    const tempMaxima = document.createElement('p');
+    tempMaxima.classList.add('style-temp');
+    tempMaxima.innerHTML = `Max: ${temp_Maxima} &#8451`;
+
+    // Temperatura minima
+    const tempMinima = document.createElement('p');
+    tempMinima.classList.add('style-temp');
+    tempMinima.innerHTML = `Min: ${temp_Minima} &#8451`;
+
+
+    const resultadoDiv = document.createElement('div');
+    resultadoDiv.appendChild(nombreCiudad)
+    resultadoDiv.appendChild(tempActual);
+    resultadoDiv.appendChild(tempMaxima);
+    resultadoDiv.appendChild(tempMinima);
+
+    resultado.appendChild(resultadoDiv);
+    
+}
+ // Puede ser función o arrowFunction
+const kelvinACentigrados = grados => parseInt(grados - 273.15);
+
+
+function limpiarHTML(){
+    while(resultado.firstChild){
+        resultado.removeChild(resultado.firstChild);
+    }
+}
 
 
 
